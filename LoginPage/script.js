af8a27b58895fa56cd8e3de48defbaa5fa2ee42a
bongmeomeo <lhquan1998@gmail.com>
2021-08-04 22:36:38 +0700

    update reset pass

--- LoginPage/script.js
+++ LoginPage/script.js
@@ -2,6 +2,7 @@
 var imgleft = document.getElementById('img-left');
 var loginform = document.getElementById('login-modal');
 var registerform = document.getElementById('register-modal');
+var resetPassForm = document.getElementById('resetpass-modal');
 // user and pass login
 var userlog = document.getElementById('user');
 var passlog = document.getElementById('pass');
@@ -14,12 +15,24 @@ var passres = document.getElementById('pass-res');
 var repassres = document.getElementById('pass-res-2');
 var typeacc = document.getElementById('select-type-acc');
 var waringRes = document.getElementById('need-input-regis');
+// user and pass reset
+var userReset = document.getElementById('user-reset');
+var passReset = document.getElementById('pass-new');
+var rePassReset = document.getElementById('pass-new-2');
+var warningReset = document.getElementById('need-input-reset');
 // Link main page
 var urlPage = "MainPage/MainPage.html"
-    // list storage account
 var account = [{ username: "admin", password: "admin", type: "admin" }];
-var checkAcc = JSON.parse(window.localStorage.getItem("account"));
+// onload html
+function onLoad() {
+    // list storage account
+    var checkAcc = JSON.parse(window.localStorage.getItem("account"));
+    if (!checkAcc) {
+        window.localStorage.setItem("account", JSON.stringify(account));
+    }
+}
 
+// switch login - register
 function switchModal() {
     if (loginform.style.display == 'none') {
         loginform.style.display = 'block';
@@ -27,6 +40,17 @@ function switchModal() {
     } else {
         loginform.style.display = 'none';
         registerform.style.display = 'block';
+    }
+}
+
+//  switch login - reset pass
+function switchModal2() {
+    if (loginform.style.display == 'none') {
+        loginform.style.display = 'block';
+        resetPassForm.style.display = 'none';
+    } else {
+        loginform.style.display = 'none';
+        resetPassForm.style.display = 'block';
     }
 }
 
@@ -56,6 +80,29 @@ function checkAccount(username, password) {
     return check;
 }
 
+function checkExistUsername(username) {
+    let check = true;
+    checkAcc = JSON.parse(window.localStorage.getItem("account"));
+    for (let i = 0; i < checkAcc.length; i++) {
+        if (checkAcc[i].username == username) {
+            check = false;
+        }
+    }
+    return check;
+}
+
+function changePassword(username, password) {
+    checkAcc = JSON.parse(window.localStorage.getItem("account"));
+    for (let i = 0; i < checkAcc.length; i++) {
+        if (checkAcc[i].username == username) {
+            checkAcc[i].password = password;
+        }
+    }
+    window.localStorage.setItem("account", JSON.stringify(checkAcc));
+
+}
+
+
 function loginSucces() {
     loader.style.display = 'block';
     setTimeout(function() {
@@ -68,17 +115,58 @@ function onSignUp() {
     let passRes = passres.value;
     let rePassRes = repassres.value;
     let type = typeacc.value;
+    let check = checkExistUsername(userRes);
     if (userRes == '' || passRes == '' || rePassRes == '') {
         waringRes.innerText = "Username and Password must fill!";
+        clearValueInput();
     } else if (passRes != rePassRes) {
         waringRes.innerText = "Retype Password not same!";
-    } else {
+        clearValueInput();
+    } else if (check == false) {
+        waringRes.innerText = "Username exits!";
+        clearValueInput();
+    } else if (check == true) {
+        clearValueInput();
         waringRes.innerText = "Register Success!";
         waringRes.style.color = "green";
-        account.push(userRes, passRes, type);
+        // account.push(userRes, passRes, type);
+        account.push({ username: userRes, password: passRes, type: type });
         window.localStorage.setItem("account", JSON.stringify(account));
         setTimeout(function() {
             switchModal();
+        }, 2000)
+
+    }
+}
+
+function clearValueInput() {
+    let input = document.getElementsByTagName('input');
+    for (let i = 0; i < input.length; i++) {
+        input[i].value = '';
+    }
+}
+
+function onResetPass() {
+    let usernameReset = userReset.value;
+    let passwordReset = passReset.value;
+    let rePasswordReset = rePassReset.value;
+    let check = checkExistUsername(usernameReset);
+    if (usernameReset == '' || passwordReset == '' || rePasswordReset == '') {
+        warningReset.innerText = "Username and Password must fill!";
+        clearValueInput();
+    } else if (passwordReset != rePasswordReset) {
+        warningReset.innerText = "Retype Password not same!";
+        clearValueInput();
+    } else if (check == true) {
+        warningReset.innerText = "Username not exits!";
+        clearValueInput();
+    } else if (check == false) {
+        clearValueInput();
+        warningReset.innerText = "Reset Password Success!";
+        warningReset.style.color = "green";
+        changePassword(usernameReset, passwordReset);
+        setTimeout(function() {
+            switchModal2();
         }, 2000)
 
     }
